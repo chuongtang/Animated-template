7fa2045532be24e23881df58f577a9cc048d807c
ChuongTang <chuong.tang@aol.com>
2021-03-25 11:25:23 -0600

    Initl commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const projectList = [
+    'UnsplashAPI-InfiniteScroll', 
+    'JokeMachine',
+    'CustomCountdown',
+    'LightDarkMode'
+  
+]
+const gitName = 'https://github.com/chuongtang/';
+const liveName = 'https://chuongtang.github.io/';
+
+
+function openLinktoNewPage (item, index){
+    window.open (`${item}${projectList[index]}`); 
+}
+
